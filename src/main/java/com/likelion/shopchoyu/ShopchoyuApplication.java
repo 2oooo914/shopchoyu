34aa5d9434cb946626cafd52195f9574f2b63b0f
yoojin <everywavyday@gmail.com>
2024-03-31 15:11:46 +0900

    file upload

---
+++ src/main/java/com/likelion/shopchoyu/ShopchoyuApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.shopchoyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopchoyuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopchoyuApplication.class, args);
+	}
+
+}
